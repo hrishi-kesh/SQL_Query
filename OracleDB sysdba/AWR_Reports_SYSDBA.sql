5db868c0236c33cb6b1976071e24c76c472ff0b9
HRISHIKESH KUMAR <HRISHIK005@GMAIL.COM>
2024-08-07 00:38:15 +0530

    Till EY interview in Shiva best practice

---
+++ OracleDB sysdba/AWR_Reports_SYSDBA.sql
@@ -0,0 +1,10 @@
+------------------DBA GENESIS youtube channel----------------------------
+---------How to Read Oracle AWR Report | Oracle Automatic Workload Repository--------
+----AWR Report:- It is useful to check performance of the database-----Automated Workload Repository----
+/*
+1. In SQL developer  -->view-->DBA-->connect SYSDBA-->Performance-->AWR-->AWR Report Viewer
+2. In SQL PLUS     -->
+
+1. AWR reports is having retension period of 7 days.
+2. It is generated at every 1 hour interval if someone ask for particular interval then also we can generate
+*/
